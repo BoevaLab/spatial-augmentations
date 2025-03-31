0136d08d8c9fa2b992a8a6ad4fd5842000cc77aa
Michel Tarnow <m.tarnow00@gmail.com>
2025-03-31 11:32:52 +0200

    refined data handling and processing for domain data

--- src/utils/preprocess_helpers.py
+++ src/utils/preprocess_helpers.py
@@ -3,18 +3,12 @@ import scanpy as sc
 import numpy as np
 import torch
 from torch_geometric.data import Data, Dataset
-from scipy.spatial import distance_matrix
+from scipy.spatial import distance_matrix, cKDTree
 import rootutils
 
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 
-from src.utils import (
-    RankedLogger,
-    extras,
-    instantiate_loggers,
-    log_hyperparameters,
-    task_wrapper,
-)
+from src.utils import RankedLogger
 
 log = RankedLogger(__name__, rank_zero_only=True)
 
@@ -24,13 +18,13 @@ def read_samples_into_dict(file_paths):
     Reads multiple AnnData objects (samples) from a list of file paths and stores them in a dictionary.
 
     Parameters:
-    ---------- 
+    ----------
     file_paths : list of str
         A list of file paths to the `.h5ad` files to be read.
 
     Returns:
     -------
-    adata_dict : dict
+    dict
         A dictionary where the keys are the filenames (without extensions) 
         and the values are the corresponding AnnData objects.
     """
@@ -43,19 +37,38 @@ def read_samples_into_dict(file_paths):
         
     return adata_dict
 
-
 def save_sample(adata, graph, output_dir, sample_name):
     """
-    Save the AnnData object and PyTorch Geometric graph for a sample.
+    Saves the AnnData object and PyTorch Geometric graph for a sample.
 
     Parameters:
-    - adata: AnnData object
-    - graph: PyTorch Geometric Data object
-    - output_dir: Directory to save the files
-    - sample_name: Name of the sample (used for filenames)
+    ----------
+    adata : AnnData
+        The AnnData object containing spatial and gene expression data.
+    graph : torch_geometric.data.Data
+        The PyTorch Geometric Data object representing the graph.
+    output_dir : str
+        Directory to save the files.
+    sample_name : str
+        Name of the sample (used for filenames).
+
+    Raises:
+    ------
+    ValueError
+        If `adata` or `graph` is None.
+
+    Returns:
+    -------
+    None
     """
     # ensure the output directory exists
     os.makedirs(output_dir, exist_ok=True)
+
+    # some checks
+    if adata is None:
+        raise ValueError("The `adata` object is None. Please provide a valid AnnData object.")
+    if graph is None:
+        raise ValueError("The `graph` object is None. Please provide a valid PyTorch Geometric Data object.")
     
     # save the AnnData object
     adata_file = os.path.join(output_dir, f"{sample_name}.h5ad")
@@ -68,8 +81,7 @@ def save_sample(adata, graph, output_dir, sample_name):
     torch.save(graph, graph_file)
     log.info(f"Saved graph to {graph_file}")
 
-
-def preprocess_sample(adata, min_cells=3, min_genes=3):
+def preprocess_sample(adata, min_cells, min_genes, n_pca_components=50):
     """
     Preprocesses a single AnnData object by applying filtering, normalization, and dimensionality reduction steps.
 
@@ -77,25 +89,12 @@ def preprocess_sample(adata, min_cells=3, min_genes=3):
     ----------
     adata : AnnData
         The AnnData object containing spatial and gene expression data.
-    min_cells : int, optional
-        The minimum number of cells in which a gene must be expressed to retain the gene. Default is 3.
-    min_genes : int, optional
-        The minimum number of genes that must be expressed in a cell to retain the cell. Default is 5.
-
-    Behavior:
-    --------
-    - Dynamically adjusts the `min_genes` threshold to be the greater of the provided `min_genes` value or 1% of the total number of genes in the dataset.
-    - Filters genes expressed in fewer than `min_cells` cells.
-    - Filters cells with fewer than the dynamically determined `min_genes` threshold.
-    - Normalizes gene expression values to a target sum of 1e5 per cell.
-    - Log-transforms the normalized gene expression values.
-    - Scales the gene expression values to unit variance and zero mean.
-    - Performs PCA to reduce dimensionality to 50 components.
-
-    Notes:
-    -----
-    - The dynamic adjustment of `min_genes` ensures that the threshold scales appropriately with the size of the dataset.
-    - This preprocessing pipeline is designed for spatial omics data but can be applied to other single-cell datasets.
+    min_cells : int
+        The minimum number of cells in which a gene must be expressed to retain the gene.
+    min_genes : int
+        The minimum number of genes that must be expressed in a cell to retain the cell.
+    n_pca_components : int, optional
+        The number of PCA components to retain during dimensionality reduction. Default is 50.
 
     Returns:
     -------
@@ -104,15 +103,13 @@ def preprocess_sample(adata, min_cells=3, min_genes=3):
     """
     total_genes = adata.n_vars
     min_genes_dynamic = max(min_genes, int(total_genes * 0.01))
-    
 
     sc.pp.filter_genes(adata, min_cells=min_cells)              # filter genes expressed in less than min_cells cells
     sc.pp.filter_cells(adata, min_genes=min_genes_dynamic)      # filter cells with less than min_genes genes expressed
     sc.pp.normalize_total(adata, target_sum=1e5)                # normalize gene expression values with a target sum per cell of 1e5
     sc.pp.log1p(adata)                                          # log transform gene expression values (log1p(x) = log(x+1))
     sc.pp.scale(adata)                                          # scale gene expression values to unit variance and zero mean
-    sc.pp.pca(adata, n_comps=50)                                # perform PCA with 50 components
-
+    sc.pp.pca(adata, n_comps=n_pca_components)                  # perform PCA with n_pca_components components
 
 def euclid_dist(t1, t2):
     """
@@ -120,29 +117,19 @@ def euclid_dist(t1, t2):
 
     Parameters:
     ----------
-    t1 : array-like
+    t1 : np.ndarray
         The first vector.
-    t2 : array-like
+    t2 : np.ndarray
         The second vector.
 
     Returns:
     -------
     float
         The Euclidean distance between the two vectors.
-
-    Notes:
-    -----
-    - The Euclidean distance is calculated as the square root of the sum of squared differences 
-      between corresponding elements of the two vectors.
-    - Both input vectors must have the same shape.
     """
-    sum = 0
-    for i in range(t1.shape[0]):
-        sum += (t1[i] - t2[i])**2
-    return np.sqrt(sum)
+    return np.linalg.norm(t1 - t2)
 
-
-def create_graph(adata, sample_name, method="knn", n_neighbors=10):
+def create_graph(adata, sample_name, method, n_neighbors):
     """
     Creates a graph representation of spatial data from an AnnData object.
 
@@ -153,18 +140,22 @@ def create_graph(adata, sample_name, method="knn", n_neighbors=10):
     sample_name : str
         The name of the sample (determined from filenames).
     method : str, optional
-        The method to use for graph construction. Options are "knn" or "pairwise". Default is "knn".
+        The method to use for graph construction. Options are "knn" or "pairwise".
         - "knn": Constructs a k-nearest neighbors graph based on spatial distances.
         - "pairwise": Constructs a fully connected graph with edge weights computed using a Gaussian kernel.
     n_neighbors : int, optional
-        The number of nearest neighbors to consider for the "knn" method. Default is 10.
+        The number of nearest neighbors to consider for the "knn" method.
 
     Returns:
     -------
-    Data : torch_geometric.data.Data
+    torch_geometric.data.Data
         A PyTorch Geometric Data object containing the graph representation of the spatial data.
-        - For "knn": Includes `x` (node features) and `edge_index` (graph connectivity).
-        - For "pairwise": Includes `x` (node features), `edge_index` (graph connectivity), and `edge_attr` (edge weights).
+
+    Raises:
+    ------
+    ValueError
+        If the AnnData object does not contain required attributes (`spatial` or `X_pca`).
+        If an invalid method is provided.
 
     Notes:
     -----
@@ -179,30 +170,29 @@ def create_graph(adata, sample_name, method="knn", n_neighbors=10):
     - The `x` attribute in the returned graph corresponds to the PCA-transformed features stored in `adata.obsm["X_pca"]`.
     - The adjacency matrix is stored in `adata.obsm['adj']` for both methods.
     """
+    # some checks
+    if "spatial" not in adata.obsm:
+        raise ValueError("The AnnData object does not contain 'spatial' coordinates in `obsm`.")
+    if "X_pca" not in adata.obsm:
+        raise ValueError("The AnnData object does not contain PCA-transformed features in `obsm['X_pca']`.")
+
     if method == "knn":
         position = adata.obsm['spatial']
-    
-        # calculate distance matrix
-        dist_mat = distance_matrix(position, position)
-        n_spot = dist_mat.shape[0]
-                
-        # find k-nearest neighbors
-        interaction = np.zeros([n_spot, n_spot])  
-        for i in range(n_spot):
-            vec = dist_mat[i, :]
-            distance = vec.argsort()
-            for t in range(1, n_neighbors + 1):
-                y = distance[t]
-                interaction[i, y] = 1
-                    
-        #transform adj to symmetrical adj
-        adj = interaction
-        adj = adj + adj.T
-        adj = np.where(adj>1, 1, adj)
         
+        # cKDTree for efficient k-NN search
+        tree = cKDTree(position)
+        _, indices = tree.query(position, k=n_neighbors + 1)  # +1 to include the point itself
+        
+        # create adjacency matrix
+        adj = np.zeros((position.shape[0], position.shape[0]), dtype=np.int32)
+        for i, neighbors in enumerate(indices):
+            adj[i, neighbors[1:]] = 1  # skip the first neighbor (itself)
+        
+        # symmetrize adjacency matrix
+        adj = np.maximum(adj, adj.T)
         adata.obsm['adj'] = adj
 
-        # convert adjacency matrix to COO format for edge index
+        # convert adjacency matrix to COO format
         edge_index = np.array(np.nonzero(adj))
         edge_index = torch.tensor(edge_index, dtype=torch.long)
         edge_weight = torch.ones(edge_index.shape[1], dtype=torch.float)
@@ -249,13 +239,54 @@ def create_graph(adata, sample_name, method="knn", n_neighbors=10):
 class SpatialOmicsDataset(Dataset):
     """
     A custom PyTorch Geometric dataset for spatial omics data.
+
+    Attributes:
+    ----------
+    samples : list
+        A list of PyTorch Geometric Data objects representing the dataset.
+
+    Methods:
+    -------
+    len() -> int
+        Returns the number of samples in the dataset.
+    get(idx: int) -> Data
+        Returns the sample at the specified index.
     """
-    def __init__(self, samples):
+    def __init__(self, samples: dict):
+        """
+        Initializes the SpatialOmicsDataset.
+
+        Parameters:
+        ----------
+        samples : dict
+            A dictionary where the keys are sample names and the values are PyTorch Geometric Data objects.
+        """
         super().__init__()
         self.samples = list(samples.values())
 
-    def len(self):
+    def len(self) -> int:
+        """
+        Returns the number of samples in the dataset.
+
+        Returns:
+        -------
+        int
+            The number of samples in the dataset.
+        """
         return len(self.samples)
 
-    def get(self, idx):
+    def get(self, idx: int) -> Data:
+        """
+        Returns the sample at the specified index.
+
+        Parameters:
+        ----------
+        idx : int
+            The index of the sample to retrieve.
+
+        Returns:
+        -------
+        torch_geometric.data.Data
+            The PyTorch Geometric Data object at the specified index.
+        """
         return self.samples[idx]
