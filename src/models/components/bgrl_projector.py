19a28796d2936516619d3fd2fb847a8281f1f8ef
Michel Tarnow <m.tarnow00@gmail.com>
2025-04-10 13:41:43 +0200

    added mm scheduler

--- src/models/components/bgrl_projector.py
+++ src/models/components/bgrl_projector.py
@@ -49,7 +49,7 @@ class BGRLProjector(nn.Module):
         A sequential container of layers, including two linear layers and a PReLU activation.
     """
 
-    def __init__(self, input_size: int, output_size: int, hidden_size: int = 64) -> None:
+    def __init__(self, input_size: int, output_size: int, hidden_size: int) -> None:
         """
         Initialize the `BGRLProjector` module.
 
@@ -59,8 +59,8 @@ class BGRLProjector(nn.Module):
             The dimensionality of the input embeddings.
         output_size : int
             The dimensionality of the output embeddings.
-        hidden_size : int, optional
-            The dimensionality of the hidden layer. Default is 64.
+        hidden_size : int
+            The dimensionality of the hidden layer.
         """
         super().__init__()
 
