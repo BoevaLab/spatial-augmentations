62bd2f9175f53cdbbe5b160ee939777d80badc7d
Michel Tarnow <m.tarnow00@gmail.com>
2025-04-15 15:30:04 +0200

    removed n neighbors from domain7 hparam search

--- scripts/domain123_hparam_baseline.sh
+++ scripts/domain123_hparam_baseline.sh
@@ -6,10 +6,10 @@
 #SBATCH --gres=gpu:rtx2080ti:1
 #SBATCH --mem=8G
 #SBATCH --cpus-per-task=1
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 
 # shellcheck disable=SC1091
 source "$HOME/.bashrc"
 conda activate augmentation
 
-srun python src/train.py -m hparams_search=domain_baseline experiment=domain123_hparam_baseline
+srun python src/train.py -m hparams_search=domain123_baseline experiment=domain123_hparam_baseline
