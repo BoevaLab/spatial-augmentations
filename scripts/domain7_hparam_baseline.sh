914e9658b52ff0bb44065ea25e6c1112801bc357
Michel Tarnow <m.tarnow00@gmail.com>
2025-04-16 13:39:46 +0200

    changed configs and scripts for domain7 hparam search

--- scripts/domain7_hparam_baseline.sh
+++ scripts/domain7_hparam_baseline.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=domain7_hparam_baseline
 #SBATCH --output=logs/slurm/domain7_hparam_baseline/%j.out
 #SBATCH -p gpu
-#SBATCH --gres=gpu:rtx2080ti:1
-#SBATCH --mem=64G
-#SBATCH --cpus-per-task=4
-#SBATCH --time=20:00:00
+#SBATCH --gres=gpu:rtx4090:1
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=6
+#SBATCH --time=40:00:00
 
 # shellcheck disable=SC1091
 source "$HOME/.bashrc"
