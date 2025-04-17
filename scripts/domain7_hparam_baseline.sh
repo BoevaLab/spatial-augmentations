35be661ec5019d3bf9dbd5fc6ebaeea5a08531bf
Michel Tarnow <m.tarnow00@gmail.com>
2025-04-17 08:54:39 +0200

    changed domain7 hparam configs to resolve cuda oom error

--- scripts/domain7_hparam_baseline.sh
+++ scripts/domain7_hparam_baseline.sh
@@ -6,10 +6,11 @@
 #SBATCH --gres=gpu:rtx4090:1
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=6
-#SBATCH --time=40:00:00
+#SBATCH --time=48:00:00
 
 # shellcheck disable=SC1091
 source "$HOME/.bashrc"
 conda activate augmentation
 
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 srun python src/train.py -m hparams_search=domain7_baseline experiment=domain7_hparam_baseline
