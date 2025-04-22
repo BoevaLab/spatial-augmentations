ad426c02648e5f051789bffbf0168a5b7b5310f8
Michel Tarnow <m.tarnow00@gmail.com>
2025-04-22 08:26:34 +0200

    adapted domain7 configs and scripts

--- scripts/domain7_train_test_baseline.sh
+++ scripts/domain7_train_test_baseline.sh
@@ -3,13 +3,14 @@
 #SBATCH --job-name=domain7_train_test_baseline
 #SBATCH --output=logs/slurm/domain7_train_test_baseline/%j.out
 #SBATCH -p gpu
-#SBATCH --gres=gpu:rtx2080ti:1
-#SBATCH --mem=64G
-#SBATCH --cpus-per-task=4
-#SBATCH --time=5:00:00
+#SBATCH --gres=gpu:rtx4090:1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=6
+#SBATCH --time=4:00:00
 
 # shellcheck disable=SC1091
 source "$HOME/.bashrc"
 conda activate augmentation
 
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 srun python src/train.py experiment=domain7_train_test_baseline
