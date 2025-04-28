096ad916933deee69f999af73e4d834b8c4e0070
Michel Tarnow <m.tarnow00@gmail.com>
2025-04-28 16:00:53 +0200

    added domain123 train_test augmentation experiment

---
+++ scripts/domain123_train_test_augmentation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=domain123_train_test_augmentation
+#SBATCH --output=logs/slurm/domain123_train_test_augmentation/%j.out
+#SBATCH -p gpu
+#SBATCH --gres=gpu:rtx2080ti:1
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:15:00
+
+# shellcheck disable=SC1091
+source "$HOME/.bashrc"
+conda activate augmentation
+
+srun python src/train.py experiment=domain123_train_test_augmentation
