51155afa09c2b5a17b0896aac711db12b019ba7f
Michel Tarnow <m.tarnow00@gmail.com>
2025-05-23 16:18:59 +0200

    request gpu with more mem for benchmarking

--- scripts/benchmark_phenotype.sh
+++ scripts/benchmark_phenotype.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=phenotype_benchmark
 #SBATCH --output=logs/slurm/benchmark/%j.out
 #SBATCH -p gpu
-#SBATCH --gres=gpu:rtx2080ti:1
+#SBATCH --gres=gpu:rtx4090:1
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1:00:00
